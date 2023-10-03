d9bbe73f4d3652ac242ba1ea2d6e95e47bcef7bd
N <34268416+ziloen@users.noreply.github.com>
2023-10-03 15:07:33 +0800

    chore: use custom uno preset

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,10 +1,15 @@
-import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
-import { defineConfig } from 'unocss/vite'
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerDirectives
+} from 'unocss'
+import { presetZiloen } from 'unocss-preset-ziloen'
 
 export default defineConfig({
   presets: [
     presetUno(),
-    presetAttributify(),
+    presetZiloen(),
     presetIcons()
   ],
   transformers: [
